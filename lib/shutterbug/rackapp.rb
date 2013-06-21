745941641821a9a19a1eaeaaf17f92eaf29db8c1
Noah Paessel <knowuh@gmail.com>
2013-06-21 15:06:44 -0400

    rackapp.rb and rackapp_spec.rb : simplify delegation to service.rb

--- lib/shutterbug/rackapp.rb
+++ lib/shutterbug/rackapp.rb
@@ -22,52 +22,36 @@ module Shutterbug
       log "initialized"
     end
 
+    def base_url(req)
+      req.POST()['base_url'] ||  req.referrer || "#{req.scheme}://#{req.host_with_port}"
+    end
 
     def do_convert(req)
-      log 'do_convert called'
-      headers = {}
-
       html     = req.POST()['content']  ||  ""
       width    = req.POST()['width']    || 1000
       height   = req.POST()['height']   ||  700
       css      = req.POST()['css']      ||  ""
-      base_url = req.POST()['base_url'] ||  req.referrer || "#{req.scheme}://#{req.host_with_port}"
 
-      log "BASE: #{base_url}"
-
-      signature = @shutterbug.convert(base_url, html, css, width, height)
+      signature = @shutterbug.convert(base_url(req), html, css, width, height)
       response_url = "#{PNG_PATH}/#{signature}"
       response_text = "<img src='#{response_url}' alt='#{signature}'>"
       return good_response(response_text,'text/plain')
     end
 
-    def do_get_png(req)
-      log 'do_get_png called'
-      sha =req.path.match(GET_PNG_REGEX)[1]
-      png_file = @shutterbug.get_png_file(sha)
-      good_response(png_file, 'image/png')
-    end
-
-    def do_get_html(req)
-      log 'do_get_html called'
-      sha =req.path.match(GET_HTML_REGEX)[1]
-      html_file = @shutterbug.get_html_file(sha)
-      good_response(html_file, 'text/html')
-    end
-
-    def do_get_shutterbug(req)
-      log 'do_get_shutterbug called'
-      shutterbug_js = @shutterbug.get_shutterbug_file
-      good_response(shutterbug_js, 'application/javascript')
-    end
-
     def call env
       req = Rack::Request.new(env)
-      return do_convert(req)  if req.path =~ CONVERT_REGEX
-      return do_get_png(req)  if req.path =~ GET_PNG_REGEX
-      return do_get_html(req) if req.path =~ GET_HTML_REGEX
-      return do_get_shutterbug(req) if req.path =~ JS_REGEX
-      return skip(env)
+      case req.path
+      when CONVERT_REGEX
+        do_convert(req)
+      when GET_PNG_REGEX
+        good_response(@shutterbug.get_png_file($1),'image/png')
+      when GET_HTML_REGEX
+        good_response(@shutterbug.get_html_file($1),'text/html')
+      when JS_PATH
+        good_response(@shutterbug.get_shutterbug_file, 'application/javascript')
+      else
+        skip(env)
+      end
     end
 
     private
@@ -86,7 +70,7 @@ module Shutterbug
     end
 
     def skip(env)
-      log 'calling app default'
+      # call the applicaiton default
       @app.call env
     end
 
--- spec/shutterbug/rackapp_spec.rb
+++ spec/shutterbug/rackapp_spec.rb
@@ -1,6 +1,15 @@
 require 'rack'
+RSpec::Matchers.define :be_happy_response do |filetype|
+  match do |actual|
+    actual[0] == 200 || actual[3] == filetype
+  end
+end
+
 describe Shutterbug::Rackapp do
-  let(:service) { mock :service }
+  let(:sha)     { "542112e"                     }
+  let(:size)    { 200                           }
+  let(:rackfile){ mock :fackfile, :size => size }
+  let(:service) { mock :service                 }
   let(:app)     { mock :app     }
   let(:post_data) do
     {
@@ -20,7 +29,7 @@ describe Shutterbug::Rackapp do
   describe "#do_convert" do
     let(:req)       { mock :req, :POST => post_data}
     it "should return a valid image url" do
-      service.should_receive(:convert).and_return("SHASIGN")
+      service.should_receive(:convert).and_return(sha)
       (resp_code,headers,content) = subject.do_convert(req)
       resp_code.should == 200
       headers['Content-Type'].should == 'text/plain'
@@ -29,89 +38,43 @@ describe Shutterbug::Rackapp do
     end
   end
 
-  describe "#do_get_png" do
-    let(:sha)      { "SHA1" }
-    let(:req)      { mock :req, :path => Shutterbug::Rackapp::PNG_PATH + "/" + sha}
-    let(:png_file) { mock :png_file, :size => 100 }
-    it "should return an image file matching sha in req of type png" do
-      service.should_receive(:get_png_file).with(sha).and_return(png_file)
-      (resp_code,headers,content) =  subject.do_get_png(req)
-      resp_code.should == 200
-      headers['Content-Type'].should == 'image/png'
-      content.should == png_file
-      headers['Content-Length'].should == '100'
-    end
-  end
-
-  describe "#do_get_html" do
-    let(:sha)       { "SHA1" }
-    let(:req)       { mock :req, :path => Shutterbug::Rackapp::HTML_PATH + "/" + sha}
-    let(:html_file) { mock :html_file, :size => 100 }
-    it "should return an image file matching sha in req of type html" do
-      service.should_receive(:get_html_file).with(sha).and_return(html_file)
-      (resp_code,headers,content) =  subject.do_get_html(req)
-      resp_code.should == 200
-      headers['Content-Type'].should == 'text/html'
-      content.should == html_file
-      headers['Content-Length'].should == '100'
-    end
-  end
-
-  describe "#do_get_shutterbug" do
-    let(:sha)       { "SHA1" }
-    let(:req)       { mock :req, :path => Shutterbug::Rackapp::JS_PATH }
-    let(:js_file  ) { mock :html_file, :size => 100 }
-    it "should return an image file matching sha in req of type javascipt" do
-      service.should_receive(:get_shutterbug_file).and_return(js_file)
-      (resp_code,headers,content) =  subject.do_get_shutterbug(req)
-      resp_code.should == 200
-      headers['Content-Type'].should == 'application/javascript'
-      content.should == js_file
-      headers['Content-Length'].should == '100'
-    end
-  end
-
-  describe "#call" do
+  describe "rounting requests in #call" do
     let(:path) { 'bogus'                   }
     let(:req)  { mock(:req, :path => path) }
-    let(:sha)  { "542112e" }
     before(:each) do
       Rack::Request.stub!(:new).and_return(req)
     end
-      # return do_convert(req)  if req.path =~ CONVERT_REGEX
-      # return do_get_png(req)  if req.path =~ GET_PNG_REGEX
-      # return do_get_html(req) if req.path =~ GET_HTML_REGEX
-      # return do_get_shutterbug(req) if req.path =~ JS_REGEX
-    describe "convert route" do
-      let(:path) { Shutterbug::Rackapp::CONVERT_PATH  }
 
+    describe "convert route" do
+      let(:path)           { Shutterbug::Rackapp::CONVERT_PATH  }
+      let(:image_response) { mock :image_response               }
       it "should route #do_convert" do
-        subject.should_receive(:do_convert, :with => req).and_return true
-        subject.call(mock).should be_true
+        subject.should_receive(:do_convert, :with => req).and_return image_response
+        subject.call(mock).should == image_response
       end
     end
 
     describe "get png route" do
       let(:path) { "#{Shutterbug::Rackapp::PNG_PATH}/#{sha}" }
       it "should route #do_get_png" do
-        subject.should_receive(:do_get_png, :with => req).and_return true
-        subject.call(mock).should be_true
+        service.should_receive(:get_png_file, :with => sha).and_return rackfile
+        subject.call(mock).should be_happy_response('image/png')
       end
     end
 
     describe "get html route" do
       let(:path) { "#{Shutterbug::Rackapp::HTML_PATH}/#{sha}" }
       it "should route #do_get_html" do
-        subject.should_receive(:do_get_html, :with => req).and_return true
-        subject.call(mock).should be_true
+        service.should_receive(:get_html_file, :with => sha).and_return rackfile
+        subject.call(mock).should be_happy_response('text/html')
       end
     end
 
     describe "get shutterbug javascipt route" do
       let(:path) { "#{Shutterbug::Rackapp::JS_PATH}" }
       it "should route #do_get_shutterbug" do
-        subject.should_receive(:do_get_shutterbug, :with => req).and_return true
-        subject.call(mock).should be_true
+        service.should_receive(:get_shutterbug_file).and_return rackfile
+        subject.call(mock).should be_happy_response('application/javascript')
       end
     end
 
