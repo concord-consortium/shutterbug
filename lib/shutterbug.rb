b3b9bbe717800648ea1649d7c29e086f814f3f76
Noah Paessel <knowuh@gmail.com>
2013-07-18 15:29:42 -0400

    shutterbug.js : load windowAddEventListeners on $(document).ready.

--- lib/shutterbug.rb
+++ lib/shutterbug.rb
@@ -1,5 +1,5 @@
 module Shutterbug
-  VERSION = "0.0.9"
+  VERSION = "0.0.10"
   autoload :Service,        "shutterbug/service"
   autoload :Rackapp,        "shutterbug/rackapp"
   autoload :Configuration,  "shutterbug/configuration"
