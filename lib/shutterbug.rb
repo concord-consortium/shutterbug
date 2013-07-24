7131e23fd4a4a0bca72c74d13cde0043e089e762
Noah Paessel <knowuh@gmail.com>
2013-07-24 17:39:52 -0400

    Version bump to 0.0.11: shutterbug.js Guard against non-json window msgs

--- lib/shutterbug.rb
+++ lib/shutterbug.rb
@@ -1,5 +1,5 @@
 module Shutterbug
-  VERSION = "0.0.10"
+  VERSION = "0.0.11"
   autoload :Service,        "shutterbug/service"
   autoload :Rackapp,        "shutterbug/rackapp"
   autoload :Configuration,  "shutterbug/configuration"
