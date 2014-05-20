7d0ec052ca40ea311ca86c5d830829e2113e78e0
Aaron Unger <aaron.unger@gmail.com>
2014-05-20 10:51:01 -0700

    Version 0.2.2

--- lib/shutterbug.rb
+++ lib/shutterbug.rb
@@ -1,5 +1,5 @@
 module Shutterbug
-  VERSION = "0.2.1"
+  VERSION = "0.2.2"
   autoload :Rackapp,        "shutterbug/rackapp"
   autoload :Configuration,  "shutterbug/configuration"
   autoload :Storage,        "shutterbug/storage"
