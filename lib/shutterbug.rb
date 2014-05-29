1dfa4ec5fbecd18e8d6541598eea98d435c3a012
Aaron Unger <aaron.unger@gmail.com>
2014-05-29 10:45:17 -0700

    Add optional size override to fix a nested iframe rendering problem
    Bump to 0.2.4

--- lib/shutterbug.rb
+++ lib/shutterbug.rb
@@ -1,5 +1,5 @@
 module Shutterbug
-  VERSION = "0.2.3"
+  VERSION = "0.2.4"
   autoload :Rackapp,        "shutterbug/rackapp"
   autoload :Configuration,  "shutterbug/configuration"
   autoload :Storage,        "shutterbug/storage"
