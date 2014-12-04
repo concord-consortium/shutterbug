f6a8889401f535c1254a92f22c905d15cb1e5226
Piotr Janik <janikpiotrek@gmail.com>
2014-12-04 13:49:08 +0000

    Fix a typo that can crash JS library
    Release v0.4.3

--- lib/shutterbug.rb
+++ lib/shutterbug.rb
@@ -1,5 +1,5 @@
 module Shutterbug
-  VERSION = "0.4.2"
+  VERSION = "0.4.3"
   autoload :Rackapp,        "shutterbug/rackapp"
   autoload :Configuration,  "shutterbug/configuration"
   autoload :Storage,        "shutterbug/storage"
